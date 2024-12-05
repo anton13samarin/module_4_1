bf7c3b6c9ee951661094461f5ea3896f8354dc11
anton13samarin <big13sav@gmail.com>
2024-12-05 16:49:31 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,22 @@
+from fake_math import divide as fa
+from true_math import divide as tr
+
+result1 = fa(69, 3)
+result2 = fa(3, 0)
+result3 = tr(49, 7)
+result4 = tr(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+result5 = fa(10,2)
+result6 = tr(10,2)
+result7 = fa(0, 5)
+result8 = tr(0,5)
+
+print(result5)
+print(result6)
+print(result7)
+print(result8)
